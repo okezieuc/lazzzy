4196c44ceb465b120d992696d245778ec58cdce5
okezieuc <53785400+okezieuc@users.noreply.github.com>
2023-04-01 00:54:02 +0100

    rewrite listener with pyaudio instead of pvrecorder
    this is because the library i'll be using for the speaker input has
    almost the same APIs as pyaudio

--- listener.py
+++ listener.py
@@ -1,9 +1,13 @@
 from dotenv import load_dotenv
 import os
 import pvporcupine
-from pvrecorder import PvRecorder
 import time
 from twilio.rest import Client
+from dotenv import load_dotenv
+import os
+import pvporcupine
+import pyaudio
+import struct
 
 load_dotenv()
 
@@ -14,7 +18,6 @@ porcupine = pvporcupine.create(
     access_key=access_key,
     keyword_paths=[os.getcwd() + "/bin/Okey-Zie_en_windows_v2_1_0.ppn"],
 )
-recoder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
 
 
 def send_twilio_message(client, recipient, from_):
@@ -32,13 +35,24 @@ try:
         os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN")
     )
 
-    recoder.start()
+    pa = pyaudio.PyAudio()
+
+    audio_stream = pa.open(
+        rate=porcupine.sample_rate,
+        channels=1,
+        format=pyaudio.paInt16,
+        input=True,
+        frames_per_buffer=porcupine.frame_length,
+    )
 
     # initialize time when keyword was last detected to 0
     name_last_detected_at = 0
 
     while True:
-        keyword_index = porcupine.process(recoder.read())
+        pcm = audio_stream.read(porcupine.frame_length)
+        pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+
+        keyword_index = porcupine.process(pcm)
         if keyword_index >= 0:
             print(f"Detected {keywords[keyword_index]}")
 
@@ -53,7 +67,6 @@ try:
 
 
 except KeyboardInterrupt:
-    recoder.stop()
+    pyaudio.Stream.close(audio_stream)
 finally:
     porcupine.delete()
-    recoder.delete()

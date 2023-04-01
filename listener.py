e13af7cf49f2dc1b4c0465ce0d2ea92cf4a0790a
okezieuc <53785400+okezieuc@users.noreply.github.com>
2023-04-01 08:33:11 +0100

    save WIP
    next up: look for how you can break up chunks of streamed data
    with a sample rate of 48,000 into bits of 16,000 each

--- listener.py
+++ listener.py
@@ -6,7 +6,7 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 import os
 import pvporcupine
-import pyaudio
+import pyaudiowpatch as pyaudio
 import struct
 
 load_dotenv()
@@ -37,6 +37,31 @@ try:
 
     pa = pyaudio.PyAudio()
 
+    # function that creates default wasapi device
+    def get_default_wasapi_device(p_audio: pyaudio.PyAudio):
+        try:  # Get default WASAPI info
+            wasapi_info = p_audio.get_host_api_info_by_type(pyaudio.paWASAPI)
+        except OSError:
+            print("Looks like WASAPI is not available on the system")
+            exit()
+
+        # Get default WASAPI speakers
+        sys_default_speakers = p_audio.get_device_info_by_index(
+            wasapi_info["defaultOutputDevice"]
+        )
+
+        if not sys_default_speakers["isLoopbackDevice"]:
+            for loopback in p_audio.get_loopback_device_info_generator():
+                if sys_default_speakers["name"] in loopback["name"]:
+                    return loopback
+                    break
+            else:
+                print(
+                    "Default loopback output device not found.\n\nRun `python -m pyaudiowpatch` to check available devices"
+                )
+                exit()
+
+    """
     audio_stream = pa.open(
         rate=porcupine.sample_rate,
         channels=1,
@@ -44,14 +69,37 @@ try:
         input=True,
         frames_per_buffer=porcupine.frame_length,
     )
+    """
+
+    # set target device for stream
+    target_device = get_default_wasapi_device(pa)
+
+    print("target: ", target_device)
+
+    print(porcupine.sample_rate)
+
+    audio_stream = pa.open(
+        format=pyaudio.paInt16,
+        # channels=target_device["maxInputChannels"],
+        channels=1,
+        rate=porcupine.sample_rate,
+        # frames_per_buffer=pyaudio.get_sample_size(pyaudio.paInt24),
+        frames_per_buffer=porcupine.frame_length,
+        input=True,
+        input_device_index=target_device["index"],
+        # stream_callback=self.callback,
+    )
 
     # initialize time when keyword was last detected to 0
     name_last_detected_at = 0
 
     while True:
+        # print('listening')
+        # pcm = audio_stream.read(pyaudio.get_sample_size(pyaudio.paInt24))
         pcm = audio_stream.read(porcupine.frame_length)
         pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
-
+        # print('checkpoint: 1')
+        # print(pcm)
         keyword_index = porcupine.process(pcm)
         if keyword_index >= 0:
             print(f"Detected {keywords[keyword_index]}")
@@ -64,6 +112,8 @@ try:
                     os.environ.get("TWILIO_RECIPIENT_PHONE_NUMBER"),
                     os.environ.get("TWILIO_FROM_PHONE_NUMBER"),
                 )
+        # else:
+        # print('not detected')
 
 
 except KeyboardInterrupt:
